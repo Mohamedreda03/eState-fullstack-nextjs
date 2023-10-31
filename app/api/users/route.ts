f4d103c8f19462651351da69928b73fadf47c503
Mohamedreda03 <mo03.reda@gmail.com>
2023-10-31 22:24:50 +0200

    delete user route

--- app/api/users/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const POST = async (req: NextRequest) => {
-  const reqBody = await req.json();
-
-  return NextResponse.json(reqBody);
-};
