9ac41e7f079b2609992d7aded1bf945360f37964
Mohamedreda03 <mo03.reda@gmail.com>
2023-11-01 00:13:27 +0200

    new fix

--- db/connectDB.ts
+++ db/connectDB.ts
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/test")
+    .connect(process.env.MONGO_URI as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("Successfully connected to MongoDB"))
     .catch((error) => console.error("Failed to connect to MongoDB:", error));
 };
